abb24fc71bf7d090976e6ad43baa96f306f9619a
tanxiaobing <hualing314159@126.com>
2019-05-12 18:02:17 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+DB_HOST = '192.168.1.2'
+DB_CONTACT_POINTS = [DB_HOST]
+DB_KEYSPACE = 'hello'
+# DB_KEYSPACE = 'test'
+# DB_KEYSPACE = 'test_dw'
+DB_USER = 'gtsb'
+DB_PASSWORD = 'gtsb'
+DB_SPLIT_SIZE_IN_MB = 128
+DB_TBL_METRIC = 'tbl_hello_world'
